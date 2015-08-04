5cb7b24ac1a4cafd36f64e003b02b9d87e286dff
Jakub <truefear@gmail.com>
2015-08-04 15:27:00 +0200

    Add bootstrap to article view page

--- app/views/view_article.blade.php
+++ app/views/view_article.blade.php
@@ -1,11 +1,12 @@
 @extends('master_layout')
 @section('content')
-<center>Article title: {{{$data["title"]}}}<br /><br />
-Article body:{{{$data["body"]}}}<br /><br />
-Comments:
+<h1>{{{$data["title"]}}}</h1><br /><br />
+<p>{{{$data["body"]}}}</p><br /><br />
+<a href="./{{{$data["id"]}}}/comment">Add comment to this article</a>
+<h3>Comments:</h3>
 @foreach ($data["detailed_comments"] as $oneComment)
-	<p>{{{$oneComment["body_comment"] }}} posted on {{{$oneComment["created_at"]}}} by: <a href="/simple_blog/public/user/{{{$oneComment["author_id"]}}}/view_profile">{{{$oneComment["author"]}}}</a></p>
+	<p class="text-left">{{{$oneComment["body_comment"] }}}</p>
+	<p class="text-right"> posted on {{{$oneComment["created_at"]}}} by: <a href="/simple_blog/public/user/{{{$oneComment["author_id"]}}}/view_profile">{{{$oneComment["author"]}}}</a></p>
 @endforeach
 <br /><br />
-<a href="./{{{$data["id"]}}}/comment">Add comment to this article</a>
 @stop
