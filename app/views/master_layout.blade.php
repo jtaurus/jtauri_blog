bb12a968d054cfb179f2713883820cd0ee20210d
Jakub <truefear@gmail.com>
2015-08-04 12:58:59 +0200

    Switch to master layout and change index page

---
+++ app/views/master_layout.blade.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-left: -150px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	
+	@if(!Auth::check())
+		<div align="right"><a href="./user/login">Login</a> | <a href="./user/create">Create account</a></div>
+	@else
+		<div align="right"><a href="./article/new">Post new article</a> | <a href="./user/logout">Logout</a></div>
+	@endif
+
+	@yield('content');
+
+	@yield('footer');
+</body>
+</html>
