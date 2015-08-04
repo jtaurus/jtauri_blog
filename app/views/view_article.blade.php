989d94757ce29454a432b17def43532b15a24952
Jakub <truefear@gmail.com>
2015-08-04 13:17:07 +0200

    Adapt view article template

--- app/views/view_article.blade.php
+++ app/views/view_article.blade.php
@@ -1,39 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
-
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
-</head>
-<body>
+@extends('master_layout')
+@section('content')
 <center>Article title: {{{$data["title"]}}}<br /><br />
 Article body:{{{$data["body"]}}}<br /><br />
 Comments:
@@ -42,6 +8,4 @@ Comments:
 @endforeach
 <br /><br />
 <a href="./{{{$data["id"]}}}/comment">Add comment to this article</a>
-</center>
-</body>
-</html>
+@stop
