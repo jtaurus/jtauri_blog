552ecda4d0021ccd1e1da903fb6d5d6b0af676d7
Jakub <truefear@gmail.com>
2015-08-04 13:43:08 +0200

    Import bootstrap

--- app/views/index_page.blade.php
+++ app/views/index_page.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <center>
 	@foreach($data["posts"] as $onePost)
-		<p><a href="./article/view/{{{$onePost["id"]}}}">{{{$onePost["title"]}}}</a></p>
+		<h4><a href="./article/view/{{{$onePost["id"]}}}">{{{$onePost["title"]}}}</a></h4>
 		<p>by: <a href="./user/{{{$onePost["author_id"]}}}/view_profile">{{{$onePost["author"]}}}</a></p>
 		<p>{{{$onePost["body"]}}}</p>
 	@endforeach
--- app/views/master_layout.blade.php
+++ app/views/master_layout.blade.php
@@ -2,37 +2,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Laravel PHP Framework</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
-
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
+
+
 		<div align="left"><a href="/simple_blog/public/">Home</a></div>
 	@if(!Auth::check())
 		<div align="right"><a href="/simple_blog/public/user/login">Login</a> | <a href="/simple_blog/public/user/create">Create account</a></div>
@@ -40,8 +14,11 @@
 		<div align="right"><a href="/simple_blog/public/article/new">Post new article</a> | <a href="/simple_blog/public/user/logout">Logout</a></div>
 	@endif
 
+	<div class="container">
 	@yield('content')
+	</div>
 
 	@yield('footer')
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
