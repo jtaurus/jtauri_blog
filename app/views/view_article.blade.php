687a9436335101930516e318eb80eaefe9c84246
Jakub <truefear@gmail.com>
2015-08-05 14:36:40 +0200

    Add wells to comments

--- app/views/view_article.blade.php
+++ app/views/view_article.blade.php
@@ -8,8 +8,8 @@
 <a href="./{{{$data["id"]}}}/comment">Add comment to this article</a>
 <h3>Comments:</h3>
 @foreach ($data["detailed_comments"] as $oneComment)
-	<p class="text-left">{{{$oneComment["body_comment"] }}}</p>
-	<p class="text-right"> posted on {{{$oneComment["created_at"]}}} by: <a href="/simple_blog/public/user/{{{$oneComment["author_id"]}}}/view_profile">{{{$oneComment["author"]}}}</a></p>
+	<div class="text-right"> posted on {{{$oneComment["created_at"]}}} by: <a href="/simple_blog/public/user/{{{$oneComment["author_id"]}}}/view_profile">{{{$oneComment["author"]}}}</a></div>
+	<div class="well" class="text-left">{{{$oneComment["body_comment"] }}}</div>
 @endforeach
 <br /><br />
 @stop
