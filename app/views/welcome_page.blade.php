c26b03448def1f0075eb762c3c23587c350d1ae7
Jakub <truefear@gmail.com>
2015-08-06 13:12:35 +0200

    Use named route in welcome page

--- app/views/welcome_page.blade.php
+++ app/views/welcome_page.blade.php
@@ -1,6 +1,6 @@
 @extends('master_layout')
 @section('content')
-<center>Welcome back, {{{$user["username"]}}}. <br /> Your e-mail address is: {{{$user["email"]}}}. <br /><a href="./logout">logout</a></center>
+<center>Welcome back, {{{$user["username"]}}}. <br /> Your e-mail address is: {{{$user["email"]}}}. <br /><a href="{{route('logout');}}">logout</a></center>
 @stop
 
 
