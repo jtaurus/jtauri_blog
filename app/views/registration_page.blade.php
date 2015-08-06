36f31c9bdb337992f0200cd951ac7321296ecaf0
Jakub <truefear@gmail.com>
2015-08-06 13:37:49 +0200

    Add bootstrap form styling to user signup page

--- app/views/registration_page.blade.php
+++ app/views/registration_page.blade.php
@@ -4,14 +4,22 @@ Create a new account
 @stop
 @section('content')
 <center>Create a new account</center>
+
 {{Form::open();}}
-{{Form::text('username', 'username');}}
+<div class="form-group">
+{{Form::label('username', 'Username');}}
+{{Form::text('username', 'username', array('class' => 'form-control'));}}
+</div>
+<div class="form-group">
+{{Form::label('email', 'E-mail')}}
+{{Form::text('email', 'email', array('class' => 'form-control', 'type' => 'email', 'placeholder' => 'email'));}}
+</div>
+<div class="form-group">
+{{Form::label('password', 'Password');}}
+{{Form::password('password', array('class' => 'form-control'));}}
+</div>
 <br />
-{{Form::text('email', 'email');}}
-<br />
-{{Form::password('password');}}
-<br />
-{{Form::submit();}}
+{{Form::submit('Submit', array('class' => 'btn btn-default'));}}
 {{Form::close();}}
 <br />
 {{{$message or ""}}}
