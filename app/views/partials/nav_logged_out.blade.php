d9ccd6897f28267cb8d4d6142e405a6b1ed752c8
Jakub <truefear@gmail.com>
2015-08-06 13:23:25 +0200

    Add dynamically generated current link to navbar in partials

--- app/views/partials/nav_logged_out.blade.php
+++ app/views/partials/nav_logged_out.blade.php
@@ -12,7 +12,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="">Current page <span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="{{URL::current()}}">Current page <span class="sr-only">(current)</span></a></li>
           <li><a href="{{route('login')}}">Login</a></li>
           <li><a href="{{route('registration')}}">Sign-up</a></li>
       </ul>
