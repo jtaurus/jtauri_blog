4618bc56d133b745dda5ac667c82fe7a872c9333
Jakub <truefear@gmail.com>
2015-08-06 13:04:27 +0200

    Link to named route when commenting

--- app/views/view_article.blade.php
+++ app/views/view_article.blade.php
@@ -8,7 +8,7 @@
 @endif
 <h1>{{{$data["title"]}}}</h1><br /><br />
 <p>{{ $data["body"] }}</p><br /><br />
-<a href="./{{{$data["id"]}}}/comment">Add comment to this article</a>
+<a href="{{route('comment_article', $data["id"]);}}">Add comment to this article</a>
 <h3>Comments:</h3>
 @foreach ($data["detailed_comments"] as $oneComment)
 	<div class="text-right"> posted on {{{$oneComment["created_at"]}}} by: <a href="/simple_blog/public/user/{{{$oneComment["author_id"]}}}/view_profile">{{{$oneComment["author"]}}}</a></div>
