5b81385cbf94d61161dcea26aa81447379005641
Jakub <truefear@gmail.com>
2015-08-08 12:18:47 +0200

    Change database schema for categories support

--- app/models/Post.php
+++ app/models/Post.php
@@ -22,4 +22,8 @@ class Post extends Eloquent{
 	public function comments(){
 		return $this->hasMany('Comment');
 	}
+
+	public function categories(){
+		return $this->belongsToMany('Category');
+	}
 }
