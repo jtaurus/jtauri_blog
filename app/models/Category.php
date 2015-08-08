c1e5d48f08b4dd8412cc6f256a8ffa5fe5b5c2ed
Jakub <truefear@gmail.com>
2015-08-08 12:09:54 +0200

    Remove unused traits from the models

--- app/models/Category.php
+++ app/models/Category.php
@@ -1,11 +1,5 @@
 <?
 
-
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
 class Category extends Eloquent{
 
 	/**
