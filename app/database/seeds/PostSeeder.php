d0eeff49f91ca1f2c55044c3b77cd60ec73f70ae
Jakub <truefear@gmail.com>
2015-08-08 13:47:07 +0200

    Add a basic posts seeder

---
+++ app/database/seeds/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+class PostSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		Post::create(array('user_id' => 1,
+			'category_id' => 1,
+			'title' => 'This is a title',
+			'body' => 'This is a body'));
+		Post::create(array('user_id' => 2,
+			'category_id' => 2,
+			'title' => 'This is the second title',
+			'body' => 'This is the second body'));
+		Post::create(array('user_id' => 3,
+			'category_id' => 3,
+			'title' => 'This is the third title',
+			'body' => 'This is the third body'));
+	}
+
+}
