02e93253134b6f516f3b1e968b80b9e2afc45326
Jakub <truefear@gmail.com>
2015-08-10 16:48:18 +0200

    Refactor view_article composer

--- app/composers.php
+++ app/composers.php
@@ -23,20 +23,20 @@ View::composer('view_article', function($view){
 	$comments = $postInstance->comments()->paginate(10);
 	$data["title"] = $postInstance["title"];
 	$data["body"] = $postInstance["body"];
-	$authorOfThePost = $postInstance->user()->get();
-	$data["author"] = $authorOfThePost[0]["username"];
-	$data["author_id"] = $authorOfThePost[0]["id"];
+	$authorOfThePost = $postInstance->user()->first();
+	$data["author"] = $authorOfThePost["username"];
+	$data["author_id"] = $authorOfThePost["id"];
 	$data["comments"] = $comments;
 	$data["detailed_comments"] = array();
 	$data["id"] = $postInstance["id"];
-	$categoryInstance = $postInstance->categories()->get();
-	$data["category_id"] = $categoryInstance[0]->id;
-	$data["category_name"] = $categoryInstance[0]->category_name;
+	$categoryInstance = $postInstance->categories()->first();
+	$data["category_id"] = $categoryInstance->id;
+	$data["category_name"] = $categoryInstance->category_name;
 	foreach($comments as $oneComment){
-			$postAuthor = $oneComment->user()->get();
+			$postAuthor = $oneComment->user()->first();
 			$moreData = $oneComment;
-			$moreData["author"] = $postAuthor[0]["username"];
-			$moreData["author_id"] = $postAuthor[0]["id"];
+			$moreData["author"] = $postAuthor["username"];
+			$moreData["author_id"] = $postAuthor["id"];
 			$data["detailed_comments"][] = $moreData;
 		}
 	$data["pagination_links"] = $comments->links();
