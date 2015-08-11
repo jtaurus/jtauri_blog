da7e8c5bb52df340128ff597c79cda5269fc0e3d
Jakub <truefear@gmail.com>
2015-08-11 17:43:14 +0200

    Add user ban button in admin panel

--- app/views/manage_users.blade.php
+++ app/views/manage_users.blade.php
@@ -5,6 +5,6 @@ Admin page - Manage users
 @section('content')
 <h3 class="text-left">Users:</h3>
 @foreach($data["users"] as $oneUser)
-	<p><a href="{{route('user_profile', $oneUser->id)}}">{{$oneUser->username}}</a></p>
+	<p><a href="{{route('user_profile', $oneUser->id)}}">{{$oneUser->username}}</a> - <a href="{{route('ban_user', $oneUser->id)}}">Ban user</a></p>
 @endforeach
 @stop
