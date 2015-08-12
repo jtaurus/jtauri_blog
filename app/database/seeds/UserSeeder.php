ccdc931580789129c36262aab30a7e18ca2efccb
Jakub <truefear@gmail.com>
2015-08-12 13:36:30 +0200

    Add user role to seeder

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -13,13 +13,16 @@ class UserSeeder extends Seeder {
 
 		User::create(array('username' => 'Jakub',
 				'email' => 'jakub@jakub.com',
-				'password' => Hash::make('123')));
+				'password' => Hash::make('123'),
+				'admin' => true));
 		User::create(array('username' => 'random_user',
 				'email' => 'random_user@random_user.com',
-				'password' => Hash::make('123')));
+				'password' => Hash::make('123'),
+				'admin' => false));
 		User::create(array('username' => 'someone88',
 				'email' => 'someone88@someone88.com',
-				'password' => Hash::make('123')));
+				'password' => Hash::make('123'),
+				'admin' => false));
 	}
 
 }
