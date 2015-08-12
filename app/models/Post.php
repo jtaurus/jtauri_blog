79efbd85564e963443674d3d6bdc283cb49668b0
Jakub <truefear@gmail.com>
2015-08-12 13:56:15 +0200

    Add isVisible method to post model.

--- app/models/Post.php
+++ app/models/Post.php
@@ -26,4 +26,8 @@ class Post extends Eloquent{
 	public function categories(){
 		return $this->belongsTo('Category', 'category_id');
 	}
+
+	public function isVisible(){
+		return $this->moderated();
+	}
 }
