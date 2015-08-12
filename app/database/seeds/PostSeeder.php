6db9112eafc1ccda34dbb111eb1886a60130aa8e
Jakub <truefear@gmail.com>
2015-08-12 13:54:06 +0200

    Add moderated field to posts table seeder.

--- app/database/seeds/PostSeeder.php
+++ app/database/seeds/PostSeeder.php
@@ -14,15 +14,23 @@ class PostSeeder extends Seeder {
 		Post::create(array('user_id' => 1,
 			'category_id' => 1,
 			'title' => 'This is a title',
-			'body' => 'This is a body'));
+			'body' => 'This is a body',
+			'moderated' => true));
 		Post::create(array('user_id' => 2,
 			'category_id' => 2,
 			'title' => 'This is the second title',
-			'body' => 'This is the second body'));
+			'body' => 'This is the second body',
+			'moderated' => true));
 		Post::create(array('user_id' => 3,
 			'category_id' => 3,
 			'title' => 'This is the third title',
-			'body' => 'This is the third body'));
+			'body' => 'This is the third body',
+			'moderated' => true));
+		Post::create(array('user_id' => 3,
+			'category_id' => 2,
+			'title' => 'This is the fourth title',
+			'body' => 'This article should no be visible by default because its moderated field is set to false',
+			'moderated' => false));
 	}
 
 }
