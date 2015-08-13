fb3613cb13a076cfcb83dcb1cfef4c4e74e7af58
Jakub <truefear@gmail.com>
2015-08-13 13:01:24 +0200

    Adapt admin panel page for moderation.

--- app/views/admin_page.blade.php
+++ app/views/admin_page.blade.php
@@ -4,6 +4,10 @@ Admin page
 @stop
 @section('content')
 <h4 class="text-left"><a href="{{route('manage_users')}}">Manage users</a></h4>
+<h3 class="text-left">Unmoderated posts</h3>
+@foreach($data["unmoderated"] as $onePost)
+<p><a href="{{route('view_article', $onePost->id)}}">{{$onePost->title}}</a> by <a href="{{route('user_profile', $onePost->user()->first()->id)}}">{{$onePost->user()->first()->username}}</a> </p> <br />
+@endforach
 <h3 class="text-left">Recent posts</h3>
 @foreach($data["posts"] as $onePost)
 <p><a href="{{route('view_article', $onePost->id)}}">{{$onePost->title}}</a> by <a href="{{route('user_profile', $onePost->user()->first()->id)}}">{{$onePost->user()->first()->username}}</a> </p> <br />
