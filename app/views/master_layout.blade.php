b5d0ac1a3a4ddcaa9dc8f6417e94b611d13e65c7
Jakub <truefear@gmail.com>
2015-08-13 17:57:18 +0200

    Use title from config.

--- app/views/master_layout.blade.php
+++ app/views/master_layout.blade.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>@yield('title')</title>
+	<title>{{ Config::get('blog.title') }} | @yield('title')</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<style>
 	  body {
