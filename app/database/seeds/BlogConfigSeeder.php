f4cb5e0ab8e94b0c9fd16bb9173397bd206e48e1
Jakub <truefear@gmail.com>
2015-08-14 20:19:10 +0200

    Add all config options to config table seeder.

--- app/database/seeds/BlogConfigSeeder.php
+++ app/database/seeds/BlogConfigSeeder.php
@@ -11,7 +11,10 @@ class BlogConfigSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		BlogConfig::create(array('name' => 'test', 'value' => 'value_of_test'));
-
+		BlogConfig::create(array('name' => 'title', 'value' => 'Jtauri blog'));
+		BlogConfig::create(array('name' => 'user_posting_enabled', 'value' => 'true'));
+		BlogConfig::create(array('name' => 'user_can_edit_posts', 'value' => 'true'));
+		BlogConfig::create(array('name' => 'post_moderation', 'value' => 'true'));
+		BlogConfig::create(array('name' => 'registration_enabled', 'value' => 'true'));
 }
 }
