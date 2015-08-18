1208b2a317e51730883ad4a05c2d0d05d8503de2
Jakub <truefear@gmail.com>
2015-08-18 16:46:11 +0200

    Refactor article_posting composer.

--- app/composers.php
+++ app/composers.php
@@ -1,8 +1,6 @@
 <?php
 
 View::composer('article_posting', function($view){
-	$userReference = Auth::user();
-	$data["username"] = $userReference->username;
 	$categoriesList = Category::all();
 	foreach($categoriesList as $oneCategory){
 		$data["categories"][$oneCategory->id] = $oneCategory->category_name;
