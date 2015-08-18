dda2b3448e4adaa345bdad373fe4eb45be73b417
Jakub <truefear@gmail.com>
2015-08-18 16:14:35 +0200

    Use title from the database based config in master layout.

--- app/views/master_layout.blade.php
+++ app/views/master_layout.blade.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>{{ Config::get('blog.title') }} | @yield('title')</title>
+	<title>{{ BlogConfig::getConfigValue('title') }} | @yield('title')</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<style>
 	  body {
